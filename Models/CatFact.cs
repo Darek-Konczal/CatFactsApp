96970cbce450362d91a9fb5ce238e7f674849ca9
darek <darekkonczal656@gmail.com>
2026-09-09 16:15:58 +0200

    Implement cat fact fetching and file storage

---
+++ Models/CatFact.cs
@@ -0,0 +1,12 @@
+using System.Text.Json.Serialization;
+
+namespace CatFactsApp.Models;
+
+public class CatFact
+{
+    [JsonPropertyName("fact")]
+    public string Fact { get; set; } = string.Empty;
+
+    [JsonPropertyName("length")]
+    public int Length { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using CatFactsApp.Models;
+
+using HttpClient client = new HttpClient();
+
+var response = await client.GetAsync("https://catfact.ninja/fact");
+
+string content = await response.Content.ReadAsStringAsync();
+
+CatFact? catFact = JsonSerializer.Deserialize<CatFact>(content);
+
+Console.WriteLine($"Fact: {catFact?.Fact}");
+Console.WriteLine($"Length: {catFact?.Length}");
+File.AppendAllText("catfacts.txt", $"{catFact?.Fact}{Environment.NewLine}");
